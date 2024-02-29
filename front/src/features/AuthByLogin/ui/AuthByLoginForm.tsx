5f584ca4ee3ad0c6a65e8cf7d62c538f8726fea1
Sheen <shneketos@gmail.com>
2024-02-29 19:54:35 +0400

    ZV

--- front/src/features/AuthByLogin/ui/AuthByLoginForm.tsx
+++ front/src/features/AuthByLogin/ui/AuthByLoginForm.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import styles from "./AuthByLoginForm.module.scss";
 export const AuthByLoginForm = () => {
-    return <div>AuthByLoginForm</div>;
+    return (
+        <div className={styles.signinForm}>
+            <div>тут дивы кидай</div>
+            <p>вот так можешь</p>
+        </div>
+    );
 };
