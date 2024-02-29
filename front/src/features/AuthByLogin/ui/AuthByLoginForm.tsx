f67fdd29c7c69b83f531a54eb0031506389702c3
harhybroo <harhy.yand3x@yandex.ru>
2024-02-29 21:23:50 +0400

    made page auth

--- front/src/features/AuthByLogin/ui/AuthByLoginForm.tsx
+++ front/src/features/AuthByLogin/ui/AuthByLoginForm.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import styles from "./AuthByLoginForm.module.scss";
+import { Logo } from "widgets/Logo/Logo";
+import { Button } from "shared/ui/Button/Button";
 export const AuthByLoginForm = () => {
     return (
         <div className={styles.signinForm}>
-            <div>тут дивы кидай</div>
-            <p>вот так можешь</p>
+            <div className={styles.signinForm_wrapper}>
+                <Logo  width={450} height={210}/>
+                <form>
+                    <input type="text" placeholder="Введите ваш email" />
+                    <input type="text" placeholder="Введите ваш пароль" />
+                </form>
+                <button>
+                    Войти
+                </button>
+              
+            </div>
+
         </div>
     );
-};
+}
 
 
 
