10ebaee28dd5085eb0a991fddf5a063d011e19ba
Sheen <shneketos@gmail.com>
2024-03-03 16:48:50 +0400

    header fix

--- front/src/widgets/Header/Header.tsx
+++ front/src/widgets/Header/Header.tsx
@@ -11,34 +11,30 @@ export const Header = (props: HeaderProps) => {
     const Client = useContext(ClientContext);
     return (
         <>
-            {Client !== null && (
-                <div className={styles.header}>
-                    <Logo width={350} height={150} />
-                    {isClient && (
-                        <div className={styles.client}>
-                            <p className={styles.client_title}>Клиент:</p>
-                            <div className={styles.client_info}>
-                                <span className={styles.client_info_name}>
-                                    {Client.familiya} {Client.name}{" "}
-                                    {Client.otchestvo}
-                                </span>
-                                <span className={styles.client_info_adress}>
-                                    {Client.address}
-                                </span>
-                                <span className={styles.client_info_tel}>
-                                    {Client.phone}
-                                </span>
-                            </div>
+            <div className={styles.header}>
+                <Logo width={350} height={150} />
+                {isClient && Client !== null && (
+                    <div className={styles.client}>
+                        <p className={styles.client_title}>Клиент:</p>
+                        <div className={styles.client_info}>
+                            <span className={styles.client_info_name}>
+                                {Client.familiya} {Client.name}{" "}
+                                {Client.otchestvo}
+                            </span>
+                            <span className={styles.client_info_adress}>
+                                {Client.address}
+                            </span>
+                            <span className={styles.client_info_tel}>
+                                {Client.phone}
+                            </span>
                         </div>
-                    )}
-                    <div className={styles.employee}>
-                        <span className={styles.employee_name}>
-                            Имя Фамилия
-                        </span>
-                        <span className={styles.employee_pos}>Должность</span>
                     </div>
+                )}
+                <div className={styles.employee}>
+                    <span className={styles.employee_name}>Имя Фамилия</span>
+                    <span className={styles.employee_pos}>Должность</span>
                 </div>
-            )}
+            </div>
         </>
     );
 };
