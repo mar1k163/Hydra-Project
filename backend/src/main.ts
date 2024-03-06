a534393322f0f3cf9956cf7cb8e7c1555f54b66b
ivan-rybakin <vanrybakin@icloud.com>
2024-03-06 09:08:32 +0400

    nest init

---
+++ backend/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: false });
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.enableCors({ credentials: true, origin: true });
+
+  await app.listen(3000);
+}
+bootstrap();
---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+}
