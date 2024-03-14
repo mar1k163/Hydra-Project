b501b20e557cfddf09d8af53cb0a3274b716dbc9
Sheen <shneketos@gmail.com>
2024-03-14 18:40:21 +0400

    fix for deploy

--- front/src/entities/ClientCard/ui/ClientCardNoRaschet/ClientCardNoRaschet.tsx
+++ front/src/entities/ClientCard/ui/ClientCardNoRaschet/ClientCardNoRaschet.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./ClientCardNoRaschet.module.scss";
 export const ClientCardNoRaschet = () => {
     return <div className={styles.msg}>У вас пока нет расчетов</div>;
