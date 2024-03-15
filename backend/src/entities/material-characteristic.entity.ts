3258e2fa6707b02f6862486f66b30bef13205451
ivan-rybakin <vanrybakin@icloud.com>
2024-03-15 14:41:22 +0400

    fix

--- backend/src/entities/material-characteristic.entity.ts
+++ backend/src/entities/material-characteristic.entity.ts
@@ -1,28 +1,28 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class MaterialCharacteristic {
-    @PrimaryGeneratedColumn()
-    id: number;
+export class MaterialCharacteristics {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column()
-    measurement_unit_id: number;
+  @Column()
+  measurement_unit_id: number;
 
-    @Column()
-    materials_id: number;
+  @Column()
+  materials_id: number;
 
-    @Column({ type: 'float', nullable: true })
-    length: number;
+  @Column({ type: "float", nullable: true })
+  length: number;
 
-    @Column({ type: 'float', nullable: true })
-    width: number;
+  @Column({ type: "float", nullable: true })
+  width: number;
 
-    @Column({ type: 'float', nullable: true })
-    thickness: number;
+  @Column({ type: "float", nullable: true })
+  thickness: number;
 
-    @Column({ type: 'float', nullable: true })
-    volume: number;
+  @Column({ type: "float", nullable: true })
+  volume: number;
 }
