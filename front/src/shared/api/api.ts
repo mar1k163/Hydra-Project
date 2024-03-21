2c13e51ab48db009dccaf4dd161f8941ae0f128c
Sheen <shneketos@gmail.com>
2024-03-21 16:25:43 +0400

    api config,zustand

--- front/src/shared/api/api.ts
+++ front/src/shared/api/api.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
+import { LOCALSTORAGE_TOKEN_KEY } from "shared/const/localstorage";
 export const $api = axios.create({
-    baseURL: "",
+    baseURL: "http://localhost:7777",
 });
 
 $api.interceptors.request.use((config) => {
+    if (typeof window !== "undefined") {
+        const token = localStorage.getItem(LOCALSTORAGE_TOKEN_KEY) || "";
+        config.headers.Authorization = "Bearer " + token;
+    }
     return config;
 });
