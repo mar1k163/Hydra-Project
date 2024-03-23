c866bac34db8591ec43fe356c80ed2965cbf54f7
WendemBox <stepanfrolov1602@gmail.com>
2024-03-23 21:22:49 +0400

    add GET and POST material_characteristics

--- backend/src/material_characteristics/material_characteristics.controller.ts
+++ backend/src/material_characteristics/material_characteristics.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { MaterialCharacteristicsService } from './material_characteristics.service';
+import { MaterialCharacteristics } from './entity/material_characteristics.entity';
 import { MaterialCharacteristicsDTO } from './dto/material_characteristics.dto';
 
-@Controller('material-characteristics')
+@Controller('material_characteristics')
 export class MaterialCharacteristicsController {
   constructor(private readonly materialCharacteristicsService: MaterialCharacteristicsService) {}
 
+  @Get()
+  async getAllMaterialCharacteristics(): Promise<MaterialCharacteristics[]> {
+    return this.materialCharacteristicsService.findAll();
+  }
+
+  @Get(':id')
+  async getMaterialCharacteristicsById(@Param('id') id: number): Promise<MaterialCharacteristics> {
+    return this.materialCharacteristicsService.findById(id);
+  }
+
   @Post()
-  async create(@Body() characteristicsData: MaterialCharacteristicsDTO) {
-    return this.materialCharacteristicsService.create(characteristicsData);
+  async createMaterialCharacteristics(@Body() materialCharacteristicsDto: MaterialCharacteristicsDTO): Promise<MaterialCharacteristics> {
+    return this.materialCharacteristicsService.create(materialCharacteristicsDto);
   }
 }
