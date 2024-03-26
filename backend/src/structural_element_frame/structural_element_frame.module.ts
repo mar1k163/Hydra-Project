86dcd00582c881532eff493eb165eb02eb123b73
WendemBox <stepanfrolov1602@gmail.com>
2024-03-26 18:28:13 +0400

    fix error

--- backend/src/structural_element_frame/structural_element_frame.module.ts
+++ backend/src/structural_element_frame/structural_element_frame.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StructuralElementFrame } from './entity/structural_element_frame.entity';
-import { StructuralElementFrameService } from './structural_element_frame.service';
-import { StructuralElementFrameController } from './structural_element_frame.controller';
+import { FrameCalculationService } from './structural_element_frame.service';
+import { FrameCalculationController } from './structural_element_frame.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([StructuralElementFrame])],
-  providers: [StructuralElementFrameService],
-  controllers: [StructuralElementFrameController],
+  providers: [FrameCalculationService],
+  controllers: [FrameCalculationController],
 })
 export class StructuralElementFrameModule {}
